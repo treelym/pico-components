5428ddd54cf5e2a9f0f9f60a63297ae0dd492bf6
treelym <trent@maijen.com>
2020-09-26 21:17:55 -0600

    Add basic Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ text }) => {
+  return (
+    <button>
+      {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string
+};
+
+export default Button;
